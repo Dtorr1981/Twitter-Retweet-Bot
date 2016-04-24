eae3bf3b8f28f862a8dc896c47f5e80fb44d634a
Dtorr1981 <D_L_Torr@hotmail.co.uk>
2016-04-24 16:13:33 +0100

    Update retweet.php

--- retweet.php
+++ retweet.php
@@ -2,10 +2,10 @@
 
 require_once('twitteroauth.php');
 
-define('CONSUMER_KEY', 'txjlQg4rRNxS7j1Yo2d9mbvTX');
-define('CONSUMER_SECRET', 'ealpSCejVP0mJ8wapZw7e5YYds3GrXcqaScjwUuJlSsnxp0X77');
-define('ACCESS_TOKEN', '722396660043804672-27sKsta28ZjOQnbSsEAwQYLOSJxzR6T');
-define('ACCESS_TOKEN_SECRET', 'JVyEXNU2NJlKcyWt8frnSH6ShuUqCXFPcdyBqZjfYHR2T');
+define('CONSUMER_KEY', 'consumer_key');
+define('CONSUMER_SECRET', 'consumer_secret');
+define('ACCESS_TOKEN', 'access_token');
+define('ACCESS_TOKEN_SECRET', 'access_token_secret');
 
 
 $conn = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET);
